1ec0c2d1c2235f36d02131958821a8146a1f7a58
yuening123456 <qw1007260923@qq.com>
2017-06-07 11:29:39 +0800

    将含有demo的类替换为Live

---
+++ app/src/main/java/cn/ucai/live/LiveConstants.java
@@ -0,0 +1,12 @@
+package cn.ucai.live;
+
+/**
+ * Created by wei on 2016/6/7.
+ */
+public class LiveConstants {
+//    public static final String EXT_IS_GIFT = "ext_is_gift";
+    public static final String CMD_GIFT = "cmd_gift";
+    public static final String EXTRA_IS_BARRAGE_MSG = "is_barrage_msg";
+    public static final String CMD_PRAISE = "cmd_live_praise";
+    public static final String EXTRA_PRAISE_COUNT = "live_praise_count";
+}
