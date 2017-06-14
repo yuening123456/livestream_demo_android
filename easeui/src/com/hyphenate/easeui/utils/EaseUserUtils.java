00e4dfb57fb72dccad6f5135c09624bba7e8c99f
yuening123456 <qw1007260923@qq.com>
2017-06-14 14:28:48 +0800

    修复 发送礼物退出直播间后在进入直播间产生的bug

---
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -0,0 +1,111 @@
+package com.hyphenate.easeui.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.hyphenate.easeui.R;
+import com.hyphenate.easeui.controller.EaseUI;
+import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.model.User;
+
+public class EaseUserUtils {
+    
+    static EaseUI.EaseUserProfileProvider userProvider;
+    
+    static {
+        userProvider = EaseUI.getInstance().getUserProfileProvider();
+    }
+    
+    /**
+     * get EaseUser according username
+     * @param username
+     * @return
+     */
+    public static EaseUser getUserInfo(String username){
+        if(userProvider != null)
+            return userProvider.getUser(username);
+
+        return null;
+}
+    public static User getAppUserInfo(String username) {
+        if (userProvider != null){
+            return userProvider.getAppUser(username);
+    }
+        return null;
+    }
+    /**
+     * set user avatar
+     * @param username
+     */
+    public static void setUserAvatar(Context context, String username, ImageView imageView){
+    	EaseUser user = getUserInfo(username);
+        if(user != null && user.getAvatar() != null){
+            try {
+                int avatarResId = Integer.parseInt(user.getAvatar());
+                Glide.with(context).load(user.getAvatar()).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView){
+        User user = getAppUserInfo(username);
+        if(user != null && user.getAvatar() != null){
+            try {
+                int avatarResId = Integer.parseInt(user.getAvatar());
+                Glide.with(context).load(user.getAvatar()).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                 Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+
+    /**
+     * set user's nickname
+     */
+    public static void setUserNick(String username,TextView textView){
+        if(textView != null){
+        	EaseUser user = getUserInfo(username);
+        	if(user != null && user.getNick() != null){
+        		textView.setText(user.getNick());
+        	}else{
+        		textView.setText(username);
+        	}
+        }
+    }
+    public static void setAppUserNick(String username,TextView textView){
+        if(textView != null){
+            User user = getAppUserInfo(username);
+            if(user != null && user.getMUserNick() != null){
+                textView.setText(user.getMUserNick());
+            }else{
+                textView.setText(username);
+            }
+        }
+    }
+    public static void setAppGiftAvatar(Context context, String giftPath, ImageView imageView){
+
+        if(giftPath!= null ){
+            try {
+                int avatarResId = Integer.parseInt(giftPath);
+                Glide.with(context).load(giftPath).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(giftPath).diskCacheStrategy(DiskCacheStrategy.ALL).
+                        placeholder(R.drawable.ease_default_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+
+}
